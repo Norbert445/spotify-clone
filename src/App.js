bd14ea7671df479c1a2dab1cc30c132b7f4e60b6
Norbi <norbimacsali445@gmail.com>
2020-10-20 11:50:54 +0200

    prihlasovanie

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login.js";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log("User is >>> ", user);
+      });
+    }
+
+    console.log("I have a token >>>> ", token);
+  }, []);
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Login.css";
+import { loginUrl } from "./spotify.js";
 
 function Login() {
   return (
     <div className="login">
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Spotify_logo_with_text.svg/1200px-Spotify_logo_with_text.svg.png" />
-      <a>LOG IN WITH SPOTIFY</a>
+      <a href={loginUrl}>LOG IN WITH SPOTIFY</a>
     </div>
   );
 }
